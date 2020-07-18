64b48e350aacb2afd058011bba0b49d2459379e2
Filip Chamier-Ciemiński <f.cieminski@gmail.com>
2020-07-18 14:35:35 +0200

    added styles and components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <div id="app">
-      hello
-  </div>
+	<div>
+		<header>
+			<div class="logos">
+				<img src="/giphy_logo.png" alt="giphy-logo" />
+				<img src="/pixabay_logo.png" alt="pixabay-logo" />
+			</div>
+			<search-form />
+			<div class="error__box">
+				<strong v-if="OneOfRequestsError">{{ OneOfRequestsError }}</strong>
+				<strong v-if="error">We are sorry, but an error occured...</strong>
+			</div>
+		</header>
+		<main>
+			<section id="container" class="giphs">
+				<strong class="loading__box">LOADING...</strong>
+			</section>
+		</main>
+	</div>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-}
+	import SearchForm from "./components/SearchForm.vue";
+	export default {
+		name: "App",
+		data() {
+			return {
+				loading: false,
+				OneOfRequestsError: null,
+				error: null
+			};
+		},
+		components: {
+			SearchForm
+		}
+	};
 </script>
 
-<style lang="scss">
-
-</style>
---
+++ client/src/components/SearchForm.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="input__container">
+		<form class="container__box">
+			<input placeholder="Cmon, search already!" class="box__input" type="text" />
+			<button type="submit" class="box__search-button">
+				<i class="material-icons material-icons--light">search</i>
+			</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "SearchForm",
+		data() {
+			return {};
+		},
+		components: {},
+		created() {},
+		computed: {},
+		methods: {}
+	};
+</script>
